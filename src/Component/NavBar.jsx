b445238962a608437631f493bc67bc425447e782
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-06-19 23:36:48 +0530

    project

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -17,7 +17,6 @@ const NavBar = () => {
             <p onClick={() =>document.getElementById("aboutUs").scrollIntoView({   behavior: "smooth", })}>About Us</p>
             <p onClick={() =>document.getElementById("ourProgram").scrollIntoView({   behavior: "smooth", })}>Our Programs</p>
             <p  onClick={() =>document.getElementById("impact").scrollIntoView({   behavior: "smooth", })}>Impact</p>
-            {/* <p>Gallery</p> */}
             <p onClick={() =>document.getElementById("contact").scrollIntoView({   behavior: "smooth", })}>Contact</p>
             <button>Donate Now</button>
         </div>
